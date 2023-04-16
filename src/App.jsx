a2a68f99c5060e9d11dbb9ac91ca84b941bdde38
Anand S <anandsatheeshk@gmail.com>
2023-04-16 18:07:10 +0530

    commented sections
    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,39 @@ import './App.css'
 function App() {
   return (
     <div>
-      <label id="break-label">Break Length</label>
-      <div id="break-decrement"><i className="fa-solid fa-arrow-down"></i></div>
-      <div id="break-length">5</div>
-      <div id="break-increment"><i className="fa-solid fa-arrow-up"></i></div>
-      <label id="session-label">Session Length</label>
-      <div id="session-decrement"><i className="fa-solid fa-arrow-down"></i></div>
-      <div id="session-lenght">25</div>
-      <div id="session-increment"><i className="fa-solid fa-arrow-up"></i></div>
+      {/* break control section start */}
+      <div>
+        <label id="break-label">Break Length</label>
+        <div id="break-decrement"><i className="fa-solid fa-arrow-down"></i></div>
+        <div id="break-length">5</div>
+        <div id="break-increment"><i className="fa-solid fa-arrow-up"></i></div>
+      </div>
+      {/* break control section end */}
+      {/* session control section start */}
+      <div>
+        <label id="session-label">Session Length</label>
+        <div id="session-decrement"><i className="fa-solid fa-arrow-down"></i></div>
+        <div id="session-lenght">25</div>
+        <div id="session-increment"><i className="fa-solid fa-arrow-up"></i></div>
+      </div>
+      {/* session control section end */}
+      {/* timer section start */}
       <div id="timer-label">Session</div>
       <div id="time-left">mm:ss</div>
+      {/* timer section end */}
+      {/* timer control section */}
       <div id="start-stop"><i class="fa-solid fa-play"></i></div>
       <div id="reset"><i class="fa-solid fa-rotate-left"></i></div>
+      {/* timer control section end */}
+      <Contact />
     </div>
   )
 }
 
 export default App
+
+function Contact() {
+  return (
+      <div>by <a target="_blank" href="https://github.com/anandxs">Anand S</a></div>
+  );
+}
