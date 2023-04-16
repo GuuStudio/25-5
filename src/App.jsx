2ef27ce9713d791b3495edc7a6f6ad83d628afe9
Anand S <anandsatheeshk@gmail.com>
2023-04-16 16:59:48 +0530

    rearranged elenents for better grouping

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
   return (
     <div>
       <label id="break-label">Break Length</label>
-      <label id="session-label">Session Length</label>
       <div id="break-decrement"><i className="fa-solid fa-arrow-down"></i></div>
-      <div id="session-decrement"><i className="fa-solid fa-arrow-down"></i></div>
-      <div id="break-increment"><i className="fa-solid fa-arrow-up"></i></div>
-      <div id="session-increment"><i className="fa-solid fa-arrow-up"></i></div>
       <div id="break-length">5</div>
+      <div id="break-increment"><i className="fa-solid fa-arrow-up"></i></div>
+      <label id="session-label">Session Length</label>
+      <div id="session-decrement"><i className="fa-solid fa-arrow-down"></i></div>
       <div id="session-lenght">25</div>
+      <div id="session-increment"><i className="fa-solid fa-arrow-up"></i></div>
       <div id="timer-label">Session</div>
       <div id="time-left">mm:ss</div>
       <div id="start-stop"><i class="fa-solid fa-play"></i></div>
