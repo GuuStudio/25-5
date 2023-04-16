2a795a0ff8dcb1803fd282b80fd121f2af65a97b
Anand S <anandsatheeshk@gmail.com>
2023-04-16 16:55:33 +0530

    replaced increment, decrement, start and reset text with font awesome icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
     <div>
       <label id="break-label">Break Length</label>
       <label id="session-label">Session Length</label>
-      <div id="break-decrement">dec</div>
-      <div id="session-decrement">dec</div>
-      <div id="break-increment">inc</div>
-      <div id="session-increment">inc</div>
+      <div id="break-decrement"><i className="fa-solid fa-arrow-down"></i></div>
+      <div id="session-decrement"><i className="fa-solid fa-arrow-down"></i></div>
+      <div id="break-increment"><i className="fa-solid fa-arrow-up"></i></div>
+      <div id="session-increment"><i className="fa-solid fa-arrow-up"></i></div>
       <div id="break-length">5</div>
       <div id="session-lenght">25</div>
       <div id="timer-label">Session</div>
       <div id="time-left">mm:ss</div>
-      <div id="start-stop">St</div>
-      <div id="reset">reset</div>
+      <div id="start-stop"><i class="fa-solid fa-play"></i></div>
+      <div id="reset"><i class="fa-solid fa-rotate-left"></i></div>
     </div>
   )
 }
